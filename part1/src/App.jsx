644654254d475dcfeebb49497b79d24a6ecb8172
Shiyi Cheng <SCHENG1@upv.edu.es>
2023-12-06 17:52:58 +0100

    12132

---
+++ part1/src/App.jsx
@@ -0,0 +1,21 @@
+// Creación de una función anónima que devuelve un div que
+// contiene un párrafo con el texto "Hello world" y lo 
+// almacena en la constante App
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p', null, 'Hello world, it is ', now.toString()
+    ),
+    React.createElement(
+      'p', null, a, ' plus ', b, ' is ', a + b
+    )
+  )
+}
+
+export default App
---
+++ part1/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+// Define un componente de React con el nombre App
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
